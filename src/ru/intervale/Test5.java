b9df79203b4803ad22395886c5b0383974efd886
Yury Prakopenka <twsega86@yandex.by>
2020-03-15 00:42:28 +0300

    qwe

--- src/ru/intervale/Test5.java
+++ src/ru/intervale/Test5.java
@@ -4,6 +4,6 @@ public class Test5 {
     public static void main(String[] args) {
         System.out.println("test5");
         System.out.println("test5");
-        System.out.println("master branch");
+        System.out.println("qwe branch");
     }
 }

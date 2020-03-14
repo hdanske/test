c88f9d02d2b325cc06cde91c7027b7c7507a9f58
Yury Prakopenka <twsega86@yandex.by>
2020-03-14 23:51:36 +0300

    edit test5 to dev

--- src/ru/intervale/Test5.java
+++ src/ru/intervale/Test5.java
@@ -4,5 +4,6 @@ public class Test5 {
     public static void main(String[] args) {
         System.out.println("test5");
         System.out.println("test5");
+        System.out.println("dev branch");
     }
 }

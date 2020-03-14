f057e6c7b86d4c0c6b0d49cdbaaee0de14b60346
Yury Prakopenka <twsega86@yandex.by>
2020-03-15 00:46:06 +0300

    1

--- src/ru/intervale/Test5.java
+++ src/ru/intervale/Test5.java
@@ -4,6 +4,6 @@ public class Test5 {
     public static void main(String[] args) {
         System.out.println("test5");
         System.out.println("test5");
-        System.out.println("adadsd branch");
+        System.out.println("a5g5gd branch");
     }
 }
